3069380b6d686f84de6596dfc7c513afb6a1e804
Jerek Dain <jerekdain@gmail.com>
2013-06-18 18:53:36 -0400

    More basic setup

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+
+// Required to make Zend Developer Tools execution time accurate.
+define('REQUEST_MICROTIME', microtime(true));
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
@@ -6,7 +12,11 @@
 chdir(dirname(__DIR__));
 
 // Setup autoloading
-require 'init_autoloader.php';
+if (!file_exists('vendor/autoload.php')) {
+    throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install`.');
+}
+
+include 'vendor/autoload.php';
 
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
